ba19038c7f783ce4072f95feed4766dd3a46e68c
Your Name <you@example.com>
2023-03-09 01:30:09 -0400

    Change

--- src/api/useFetch.js
+++ src/api/useFetch.js
@@ -5,7 +5,7 @@ export function useFetch(nameCity) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&cnt=4&APPID=fa3a81e49de01990ae6f68ff5b4ae20c`;
+    const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&cnt=4&APPID=${process.env.WEATHER_API_KEY}`;
     setLoading(true);
     axios({
       method: "get",
